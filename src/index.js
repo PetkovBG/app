c533f7f84cb77ed1ca70a19d85380f1e4b1ca2ff
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-02-22 11:30:45 +0200

    Updated Board, 'X' and 'O' now take turns

--- src/index.js
+++ src/index.js
@@ -14,23 +14,24 @@ import './index.css';
     }
 
 
-   
-    
-  
-  
+
   class Board extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+        xIsNext: !this.state.xIsNext,
+        })
     }
 
     renderSquare(i) {
