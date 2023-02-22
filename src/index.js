d85c810050e3fe62dc7cc609344f316b471084e0
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-02-22 10:34:20 +0200

    Added constructor to Square class

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
       return (
-        <button className="square">
+        <button className="square" onClick={ () => console.log('click')} >
             {this.props.value}
         </button>
       );
