a2347ba011e8b7645d0dfbf53574fbe94bedf152
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-02-22 12:08:32 +0200

    Moved handleClick method from Board to Game

--- src/index.js
+++ src/index.js
@@ -19,17 +19,7 @@ import './index.css';
 
   
 
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if(calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-        xIsNext: !this.state.xIsNext,
-        })
-    }
+  
 
     renderSquare(i) {
       return (
@@ -52,7 +42,7 @@ import './index.css';
   
       return (
         <div>
-          <div className="status">{status}</div>
+        
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -85,7 +75,36 @@ import './index.css';
         };
     }
 
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = this.state.squares.slice();
+        if(calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+        xIsNext: !this.state.xIsNext,
+        })
+    }
+
     render() {
+
+        const history = this.state.history;
+        const current = history[history.length -1];
+        const winner = calculateWinner(current.squares);
+        
+        let status;
+
+        if(winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
       return (
         <div className="game">
           <div className="game-board">
@@ -95,7 +114,7 @@ import './index.css';
             />
           </div>
           <div className="game-info">
-            <div>{status}</div>
+            <div>{ status }</div>
             <ol>{/* TODO */}</ol>
           </div>
         </div>
