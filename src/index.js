214d73994086cd773eab58d8efd7a4ae3d4fc418
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-02-22 10:45:14 +0200

    Set state to X

--- src/index.js
+++ src/index.js
@@ -13,8 +13,10 @@ class Square extends React.Component {
 
     render() {
       return (
-        <button className="square" onClick={ () => console.log('click')} >
-            {this.props.value}
+        <button className="square" 
+        onClick={ () => this.setState({value: 'X'})} >
+            
+            {this.state.value}
         </button>
       );
     }
